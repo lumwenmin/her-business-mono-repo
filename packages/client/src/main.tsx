3c251688d5a6f0607b226220d6aaf6aece91b0bc
clara <clara@vendora.io>
2025-02-15 22:26:03 +0800

    added trpc and tanstack query

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -2,8 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "@/components/ui/provider";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { routeTree } from "./routeTree.gen";
+import { trpc } from "./trpc";
+import { trpcClient } from "./apiClient";
 
 const router = createRouter({ routeTree });
 
@@ -13,10 +15,16 @@ declare module "@tanstack/react-router" {
   }
 }
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <trpc.Provider client={trpcClient} queryClient={queryClient}>
+          <RouterProvider router={router} />
+        </trpc.Provider>
+      </QueryClientProvider>
     </Provider>
   </StrictMode>
 );
--- packages/client/src/trpc.ts
+++ packages/client/src/trpc.ts
@@ -1,6 +1,4 @@
 import type { AppRouter } from "shared/src/appRouter";
 import { createTRPCReact } from "@trpc/react-query";
 
-export const trpc = createTRPCReact<AppRouter>({
-  // optionally, you can provide custom options here
-});
+export const trpc = createTRPCReact<AppRouter>();
